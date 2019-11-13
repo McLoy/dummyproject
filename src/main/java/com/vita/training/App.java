e8b70d9a584df3583c6318df6458a6a441ccd386
McLoy <tkachenko.vlad@hotmail.com>
2019-11-13 19:07:00 +0200

    Init commit

---
+++ src/main/java/com/vita/training/App.java
@@ -0,0 +1,8 @@
+package com.vita.training;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Заработало!");
+        System.out.println("Application works!");
+    }
+}
